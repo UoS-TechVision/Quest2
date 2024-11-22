58c9110ac8365457803c0f27621326494bcacee5
ReuBird23 <150720326+ReuBird23@users.noreply.github.com>
2024-11-22 22:21:02 +0000

    Generate Linear Level

--- Quest2/Assets/Scripts/Generate.cs
+++ Quest2/Assets/Scripts/Generate.cs
@@ -1,11 +1,25 @@
+using System;
 using UnityEngine;
 
 public class Generate : MonoBehaviour
 {
     [SerializeField]
+    int nRooms;
+    [SerializeField]
+    GameObject StartRoom;
+    [SerializeField]
     GameObject[] Rooms;
+    [SerializeField]
+    MeshRenderer StartRenderer;
+    Vector3 size;
     void Start()
     {
-        
+        size = StartRenderer.bounds.size;
+        Instantiate(StartRoom, new Vector3(0, 0, 0), new Quaternion(0,180,0,1), this.transform);
+        for (int i = 1; i <= nRooms; i++)
+            {
+                Instantiate(Rooms[0], new Vector3(i * size.x, 0, 0), Quaternion.identity, this.transform);
+            }
+        Instantiate(StartRoom, new Vector3((nRooms + 1) * size.x, 0, 0), Quaternion.identity, this.transform);
     }
 }
