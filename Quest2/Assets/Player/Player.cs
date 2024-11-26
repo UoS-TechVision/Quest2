a17d8557fe54a721d9d0fcf14ff5837d0a9ffce4
ReuBird23 <150720326+ReuBird23@users.noreply.github.com>
2024-11-26 21:20:15 +0000

    Added Player

---
+++ Quest2/Assets/Player/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController characterController;
+    [SerializeField]
+    float speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        characterController.Move(move*Time.deltaTime*speed);
+    }
+}
