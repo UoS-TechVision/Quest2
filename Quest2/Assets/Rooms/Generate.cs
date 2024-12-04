e654e3482bc3464a8493580918722df8e18c618a
ReuBird23 <150720326+ReuBird23@users.noreply.github.com>
2024-12-04 16:13:23 +0000

    Added coherence

--- Quest2/Assets/Rooms/Generate.cs
+++ Quest2/Assets/Rooms/Generate.cs
@@ -12,14 +12,26 @@ public class Generate : MonoBehaviour
     [SerializeField]
     MeshRenderer StartRenderer;
     Vector3 size;
+    [SerializeField]
+    float cohesion;
     void Start()
     {
         size = StartRenderer.bounds.size;
         Instantiate(StartRoom, new Vector3(0, 0, 0), new Quaternion(0,180,0,1), this.transform);
+
+        GameObject previousRoom = Rooms[UnityEngine.Random.Range(0, Rooms.Length)];
         for (int i = 1; i <= nRooms; i++)
             {
-                Instantiate(Rooms[UnityEngine.Random.Range(0,4)], new Vector3(i * size.x, 0, 0), Quaternion.identity, this.transform);
+                if (UnityEngine.Random.Range(0f, 1f) < cohesion)
+                {
+                    Instantiate(previousRoom, new Vector3(i * size.x, 0, 0), Quaternion.identity, this.transform);
+                }
+                else
+                {
+                    previousRoom = Instantiate(Rooms[UnityEngine.Random.Range(0, Rooms.Length)], new Vector3(i * size.x, 0, 0), Quaternion.identity, this.transform);
+                }
             }
+            
         Instantiate(StartRoom, new Vector3((nRooms + 1) * size.x, 0, 0), Quaternion.identity, this.transform);
     }
 }
