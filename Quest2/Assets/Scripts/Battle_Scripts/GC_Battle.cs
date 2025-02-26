3e86862cf219927477e4528ee0e0ceb13f2377e8
Tanay <tvpatel1@sheffield.ac.uk>
2025-02-26 16:06:22 +0000

    initalised battle controller

---
+++ Quest2/Assets/Scripts/Battle_Scripts/GC_Battle.cs
@@ -0,0 +1,87 @@
+/**
+* Battle Scene Game Controller
+* Controls the flow of battle
+*/
+using UnityEngine;
+
+public enum BattleState {Start, PlayerTurn, EnemyTurn, WON, LOST}
+
+public class GC_Battle : MonoBehaviour
+{
+
+    public BattleState state; 
+
+    public GameObject playerObj;
+    public GameObject enemyObj;
+
+    public Transform playerSpawnPos;
+    public Transform enemySpawnPos; 
+
+    Unit playerUnit;
+
+    Unit enemyUnit; 
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Loaded with Scaling of " + BattleInfo.difficultyScaling); 
+        state = BattleState.Start;
+        StartCoroutine(SetUpBattle());
+        
+    }
+
+
+    IEnumerator SetUpBattle()
+    {
+        GameObject playerGO = Instantiate(playerObj, playerSpawnPos);
+        playerUnit = playerGO.getComponent<Unit>();
+
+
+        GameObject enemyGO = Instantiate(enemyObj, enemySpawnPos);
+        enemyUnit = enemyObj.GetComponent<Unit>(); 
+        
+        yield return new WaitForSeconds(2f); 
+        state = BattleState.PlayerTurn; 
+        PlayerTurn();
+    } 
+
+
+
+    IEnumerator PlayerAttack(){
+        // Damage Enemy
+
+
+        yield return new WaitForSeconds(2f);
+
+        // CHeck if enemy is dead
+        // Change State based on what happened
+    }
+
+
+    void PlayerSelectAttack(){
+        
+    }
+
+    void PlayerTurn()
+    {
+        // Set player dialogue text
+    }
+
+
+    void onAttackButton(){
+        if (state != BattleState.PlayerTurn){
+            return;
+        }
+
+        int damage = PlayerSelectAttack()
+        
+        StartCoroutine(PlayerAttack())
+    }
+
+
+
+
+
+}
---
+++ Quest2/Assets/Scripts/Battle_Scripts/Unit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+    public int maxHP;
+    public int currentHP;
+
+    public int unitDamage;
+}
+
+
+  
