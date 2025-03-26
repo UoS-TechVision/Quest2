d61ce143688648b79abdc830373e8b1da77ae970
baorii <john.guibao127@gmail.com>
2025-03-26 19:22:18 +0000

    Code to change to battle scene - still fixing, worked once

--- Quest2/Assets/Scripts/Player.cs
+++ Quest2/Assets/Scripts/Player.cs
@@ -28,9 +28,57 @@ public class Player : MonoBehaviour
 
         characterController.Move(move * (Time.deltaTime * speed));
         Debug.unityLogger.Log(stats.ToString());
-        Collider[] hitArr = Physics.OverlapSphere(this.transform.position, 2f, 6);
+        Collider[] hitArr = Physics.OverlapSphere(this.transform.position, 5f, 6);
+        Debug.Log($"Detected {hitArr.Length} colliders in OverlapSphere.");
+
+        if (hitArr.Length == 0)
+        {
+            return;
+        }
+
+        foreach (Collider hit in hitArr)
+        {
+            if (hit.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                Debug.unityLogger.Log("Monster hit!");
+
+                SceneChangeTrigger scriptComponent = hit.gameObject.GetComponent<SceneChangeTrigger>();
+                scriptComponent.battleScene = "devBattle";
+                scriptComponent.specifiedMonster = hit.gameObject;
+
+                // GC_Overworld gcOverworld = Object.FindAnyObjectByType<GC_Overworld>();
+
+                // if (gcOverworld == null)
+                // {
+                //     Debug.unityLogger.Log("GC_Overworld not found!");
+                //     return;
+                // }
+                // else {
+                //     Debug.unityLogger.Log("GC_Overworld found!");
+                //     gcOverworld.TransitionToBattle();
+                //     Destroy(hit.gameObject);
+                // }
+                
+            }
+        }
 
     }
+
+    private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("OnTrigger called!");
+
+            if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                Debug.Log("Enemy entered trigger!");
+                SceneChangeTrigger scriptComponent = other.gameObject.GetComponent<SceneChangeTrigger>();
+                if (scriptComponent != null)
+                {
+                    scriptComponent.battleScene = "devBattle";
+                    scriptComponent.specifiedMonster = other.gameObject;
+                }
+            }
+        }
 
 
 
