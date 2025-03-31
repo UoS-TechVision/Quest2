246ee9b88ca5223606ca18f0e4275046f769722d
baorii <john.guibao127@gmail.com>
2025-04-01 00:28:18 +0100

    Player and enemy data are transferred to battle scene during transition

--- Quest2/Assets/Scripts/Player.cs
+++ Quest2/Assets/Scripts/Player.cs
@@ -48,19 +48,62 @@ public class Player : MonoBehaviour
     //     }
     // }
 
+    private GameObject specifiedMonster;
+    private string battleScene;
+
     private void OnTriggerEnter(Collider other) {
         Debug.Log("OnTriggerEnter called with: " + other.gameObject.name);
 
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
             Debug.Log("Enemy entered trigger!");
-            SceneChangeTrigger scriptComponent = other.gameObject.GetComponent<SceneChangeTrigger>();
-            if (scriptComponent != null) {
-                scriptComponent.battleScene = "devBattle";
-                scriptComponent.specifiedMonster = other.gameObject;
-            }
+            battleScene = "devBattle";
+            specifiedMonster = other.gameObject;
+            DontDestroyOnLoad(specifiedMonster);
+            Debug.Log($"Collided with monster: {specifiedMonster.name}");
 
-            // Got simple scene change working, The above is for more complex scene change
+            // Transfer transferObj = GameObject.FindFirstObjectByType<Transfer>();
+            // if (transferObj != null)
+            // {
+            //     //Pass transferObj player reference to the Battle Controller
+            //     transferObj.setPlayerObj(GameObject.FindWithTag("Player"));
+            //     transferObj.setEnemyObj(specifiedMonster);
+            //     Debug.Log($"Transfer monster: {specifiedMonster.name}");
+            // }
+            // else
+            // {
+            //     Debug.LogWarning("Warning: Could not find Battle Controller in Battle Scene!");
+            // }
+
+            SceneManager.sceneLoaded += OnBattleSceneLoaded;
+            //Load the Battle Scene
+            Debug.Log("Transitioning to Battle Scene!");
+            // // Got simple scene change working, The above is for more complex scene change
             SceneManager.LoadScene("devBattle", LoadSceneMode.Single);
+        }
+    }
+
+    private void OnBattleSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Battle Scene Loaded!");
+
+        if (scene.name == battleScene)
+        {
+
+            //Unregister the callback
+            SceneManager.sceneLoaded -= OnBattleSceneLoaded;
+            //Get the Battle Controller
+            Transfer transferObj = GameObject.FindFirstObjectByType<Transfer>();
+            if (transferObj != null)
+            {
+                //Pass transferObj player reference to the Battle Controller
+                transferObj.setPlayerObj(GameObject.FindWithTag("Player"));
+                transferObj.setEnemyObj(specifiedMonster);
+                Debug.Log($"Transfer monster: {specifiedMonster.name}");
+            }
+            else
+            {
+                Debug.LogWarning("Warning: Could not find Battle Controller in Battle Scene!");
+            }
         }
     }
 }
